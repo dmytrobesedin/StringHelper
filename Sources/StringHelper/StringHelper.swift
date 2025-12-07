f2cf35cdc6dc4eb2875df0c722c51e6e72326a85
dmytrobesedin <dmytro.besedin@carecam.ai>
2025-12-07 23:42:05 +0200

    convert static methods to instance methods

--- Sources/StringHelper/StringHelper.swift
+++ Sources/StringHelper/StringHelper.swift
@@ -5,10 +5,12 @@ import Foundation
 
 public struct StringHelper {
 
+    public init() { }
+    
     ///  Check if a string is a palindrome
     /// - Parameter input: The string to check
     /// - Returns: True if the string is a palindrome, false otherwise
-    public static func isPalindrome(_ input: String) -> Bool {
+    public func isPalindrome(_ input: String) -> Bool {
         guard !input.isEmpty else { return false }
         let cleaned = input.lowercased().filter { $0.isLetter || $0.isNumber }
         return cleaned == String(cleaned.reversed())
@@ -17,7 +19,7 @@ public struct StringHelper {
     /// Count the number of vowels in a string
     /// - Parameter input: The string to check
     /// - Returns: The number of vowels in the string
-    public static func countVowels(_ input: String) -> Int {
+    public func countVowels(_ input: String) -> Int {
         guard !input.isEmpty else { return 0 }
         let vowels = "aeiou"
         return input.lowercased().filter { vowels.contains($0) }.count
--- Tests/StringHelperTests/StringHelperTests.swift
+++ Tests/StringHelperTests/StringHelperTests.swift
@@ -2,20 +2,31 @@ import XCTest
 @testable import StringHelper
 
 final class StringHelperTests: XCTestCase {
-
+    var tug: StringHelper!
+    
+    override func setUp() {
+        super.setUp()
+        tug = StringHelper()
+    }
+    
+    override func tearDown() {
+        tug = nil
+        super.tearDown()
+    }
+    
     func test_Palindrome() {
-        XCTAssertTrue(StringHelper.isPalindrome("level"))
+        XCTAssertTrue(tug.isPalindrome("level"))
     }
 
     func test_isNotPalindrome() {
-        XCTAssertFalse(StringHelper.isPalindrome("Swift"))
+        XCTAssertFalse(tug.isPalindrome("Swift"))
     }
     
     func test_VowelCount_isTwo() {
-        XCTAssertEqual(StringHelper.countVowels("hello"), 2)
+        XCTAssertEqual(tug.countVowels("hello"), 2)
     }
     
     func test_VowelCount_isNotThree() {
-        XCTAssertFalse(StringHelper.countVowels("Bye") == 3)
+        XCTAssertFalse(tug.countVowels("Bye") == 3)
     }
 }
